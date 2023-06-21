8eb4761a62e65986bc6e936cde8412f89d16f364
MerUribe <meruribe@gmail.com>
2023-06-21 19:44:42 -0300

    creacion try catch. Agrego async await

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
-const cargarPeliculas = () => {
-    const repuesta = fetch('https://api.themoviedb.org/3/movie/11?api_key=0d9435a80057b6766ea079f2673d5514')
+const cargarPeliculas = async() => {
+
+    try {
+        const respuesta = await fetch('https://api.themoviedb.org/3/movie/11?api_key=0d9435a80057b6766ea079f2673d5514&language=es-AR');
     
-    console.log(respuesta);
+        console.log(respuesta);
+
+        const datos = await respuesta.json();
+
+        console.log(datos.title);
+    } catch(error) {
+        console.log(error);
+    }
+
 }
 
 cargarPeliculas();
